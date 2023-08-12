98db7da313e4da9599a0d74dec42b618a21602d5
Arthur <arthur.perigno@gmail.com>
2023-08-12 22:55:29 +0200

    Debut rework game feel

--- Projet Bird's Scourge/Assets/Scripts/UIBattleAttack.cs
+++ Projet Bird's Scourge/Assets/Scripts/UIBattleAttack.cs
@@ -18,20 +18,56 @@ public class UIBattleAttack : MonoBehaviour
     [Range(0f, 5f)] public float attackedShakeDuration;
     [Range(0f, 15f)] public float attackedShakeAmplitude;
 
+    [Header("Movement")]
+    [Range(-150f, 150f)] public float attackerMovement = 0;
+    [Range(0f, 2f)] public float attackerMovementDuration = 0;
+    [Range(-150f, 150f)] public float attackedMovement = 0;
+    [Range(0f, 2f)] public float attackedMovementDuration = 0;
+    private float originalXLeft;
+    private float originalXRight;
+
+    [Header("Rotation")]
+    [Range(-20f, 20f)] public float attackerRotation = 0;
+    [Range(0f, 2f)] public float attackerRotationDuration = 0;
+    [Range(-20f, 20f)] public float attackedRotation = 0;
+    [Range(0f, 2f)] public float attackedRotationDuration = 0;
+
+    [Header("Scale")]
+    [Range(0f, 2f)] public float attackerScale = 1;
+    [Range(0f, 2f)] public float attackerScaleDuration = 0;
+    [Range(0f, 2f)] public float attackedScale = 1;
+    [Range(0f, 2f)] public float attackedScaleDuration = 0;
+
+    [Header("Colors")]
+    public Color colorStart;
+    public Color colorStandard;
+    public Color colorEnd;
+    public Color colorDamage;
+    public Color colorHeal;
+    public Color colorSummon;
+    [Range(0f, 2f)] public float fadeColorStartDuration = 0.2f;
+    [Range(0f, 2f)] public float fadeColorEndDuration = 0.1f;
+    [Range(0f, 2f)] public float flickerColorDuration = 0.3f;
+
+
     [Header("ParametersText")] 
     public Color colorNormalAttack;
     public Color colorMissAttack;
     public Color colorCritAttack;
     public Color colorNormalHeal;
     public Color colorCritHeal;
-    public Color colorSummon;
+    public Color colorSummonText;
     
     [Header("References")] 
     public TextMeshProUGUI damageNumber;
     public RectTransform attackUI;
+    public RectTransform leftCharaParent;
+    public RectTransform rightCharaParent;
     public Image leftChara;
     public Image rightChara;
     public Image attackFond;
+
+
 
     public void Start()
     {
@@ -42,12 +78,24 @@ public class UIBattleAttack : MonoBehaviour
     // SETUP EVERY ALPHAS WHEN THE SCENE START
     public void AttackUISetup()
     {
+        originalXLeft = leftCharaParent.transform.position.x;
+        originalXRight = rightCharaParent.transform.position.x;
+
         attackFond.DOFade(0, 0);
-        
-        leftChara.DOFade(0, 0);
-        rightChara.DOFade(0, 0);
+
+        leftChara.material.SetFloat("_Alpha", 0);
+        rightChara.material.SetFloat("_Alpha", 0);
+
+        leftChara.material.SetColor("_Color", colorStart);
+        rightChara.material.SetColor("_Color", colorStart);
 
         attackUI.DORotate(Vector3.zero, 0);
+        leftCharaParent.DORotate(Vector3.zero, 0);
+        rightCharaParent.DORotate(Vector3.zero, 0);
+
+        leftCharaParent.DOScale(Vector3.one, 0);
+        rightCharaParent.DOScale(Vector3.one, 0);
+
         attackUI.localScale = Vector3.one;
         damageNumber.rectTransform.localScale = Vector3.one;
     }
@@ -62,7 +110,7 @@ public class UIBattleAttack : MonoBehaviour
 
         TextFeel(leftOrigin, miss, crit, damage, false, false);
 
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(1.3f);
 
         StartCoroutine(EndFeel());
     }
@@ -114,8 +162,20 @@ public class UIBattleAttack : MonoBehaviour
 
         attackFond.DOFade(0.8f, apparitionFadeDuration);
         
-        leftChara.DOFade(1, apparitionFadeDuration);
-        rightChara.DOFade(1, apparitionFadeDuration);
+        /*leftChara.DOFade(1, apparitionFadeDuration);
+        rightChara.DOFade(1, apparitionFadeDuration);*/
+
+        float fadeLeft = leftChara.material.GetFloat("_Alpha");
+        DOTween.To(() => fadeLeft, x => fadeLeft = x, 1, fadeColorStartDuration)
+            .OnUpdate(() => {
+                leftChara.material.SetFloat("_Alpha", fadeLeft);
+            });
+
+        float fadeRight = rightChara.material.GetFloat("_Alpha");
+        DOTween.To(() => fadeRight, x => fadeRight = x, 1, fadeColorStartDuration)
+            .OnUpdate(() => {
+                rightChara.material.SetFloat("_Alpha", fadeRight);
+            });
     }
 
 
@@ -124,19 +184,29 @@ public class UIBattleAttack : MonoBehaviour
     {
         if (leftOrigin)
         {
-            /*leftChara.rectTransform.DOMoveX(leftChara.rectTransform.position.x + 30, 0.5f);
-            rightChara.rectTransform.DOMoveX(rightChara.rectTransform.position.x - 15, 0.5f);*/
+            leftChara.rectTransform.DOShakePosition(attackerShakeDuration, new Vector3(1, 1, 0) * attackerShakeAmplitude);
+            rightChara.rectTransform.DOShakePosition(attackedShakeDuration, new Vector3(1, 1, 0) * attackedShakeAmplitude);
 
-            leftChara.rectTransform.DOShakePosition(attackerShakeDuration, attackerShakeAmplitude);
-            rightChara.rectTransform.DOShakePosition(attackedShakeDuration, attackedShakeAmplitude);
+            leftCharaParent.DOMoveX(leftCharaParent.position.x + attackerMovement, attackerMovementDuration).SetEase(Ease.OutQuint);
+            rightCharaParent.DOMoveX(rightCharaParent.position.x + attackedMovement, attackedMovementDuration);
 
-            if (unifiedShake)
-            {
-                attackUI.DOShakePosition(1f, 10f);
-            }
+            leftCharaParent.DORotate(leftCharaParent.rotation.eulerAngles + new Vector3(0, 0, attackerRotation), attackerRotationDuration);
+            rightCharaParent.DORotate(rightCharaParent.rotation.eulerAngles + new Vector3(0, 0, attackedRotation), attackedRotationDuration);
 
-            attackUI.DOScale(attackUI.localScale * 1.1f, 0.2f);
-            attackUI.DORotate(new Vector3(0, 0, -5), 0.1f);
+            leftCharaParent.DOScale(Vector3.one * attackerScale, attackerScaleDuration);
+            rightCharaParent.DOScale(Vector3.one * attackedScale, attackedScaleDuration);
+
+            Color colorLeft = leftChara.material.GetColor("_Color");
+            DOTween.To(() => colorLeft, x => colorLeft = x, colorStandard, fadeColorStartDuration)
+                .OnUpdate(() => {
+                    leftChara.material.SetColor("_Color", colorLeft);
+                });
+
+            Color colorRight = rightChara.material.GetColor("_Color");
+            DOTween.To(() => colorRight, x => colorRight = x, colorStandard, fadeColorStartDuration)
+                .OnUpdate(() => {
+                    rightChara.material.SetColor("_Color", colorRight);
+                });
         }
 
         else
@@ -147,13 +217,10 @@ public class UIBattleAttack : MonoBehaviour
             rightChara.rectTransform.DOShakePosition(attackerShakeDuration, attackerShakeAmplitude);
             leftChara.rectTransform.DOShakePosition(attackedShakeDuration, attackedShakeAmplitude);
 
-            if (unifiedShake)
-            {
-                attackUI.DOShakePosition(1f, 10f);
-            }
-            
+
+            /*attackUI.DOShakePosition(1f, 10f);
             attackUI.DOScale(attackUI.localScale * 1.1f, 0.2f);
-            attackUI.DORotate(new Vector3(0, 0, -5), 0.1f);
+            attackUI.DORotate(new Vector3(0, 0, -5), 0.1f); */
         }
     }
     
@@ -167,7 +234,7 @@ public class UIBattleAttack : MonoBehaviour
             damageNumber.transform.rotation = new Quaternion(0, 0, -7,0);
         
             damageNumber.text = "Summoned";
-            damageNumber.color = colorSummon;
+            damageNumber.color = colorSummonText;
             
             damageNumber.DOFade(1, 0.07f);
             damageNumber.transform.DOScale(damageNumber.transform.localScale * 1.1f, 0.2f);
@@ -262,14 +329,26 @@ public class UIBattleAttack : MonoBehaviour
     public IEnumerator EndFeel()
     {
         attackFond.DOFade(0f, 0.1f);
-        
-        leftChara.DOFade(0, 0.1f);
-        rightChara.DOFade(0, 0.1f);
-        
+
+        float fadeLeft = leftChara.material.GetFloat("_Alpha");
+        DOTween.To(() => fadeLeft, x => fadeLeft = x, 0, fadeColorEndDuration)
+            .OnUpdate(() => {
+                leftChara.material.SetFloat("_Alpha", fadeLeft);
+            });
+
+        float fadeRight = rightChara.material.GetFloat("_Alpha");
+        DOTween.To(() => fadeRight, x => fadeRight = x, 0, fadeColorEndDuration)
+            .OnUpdate(() => {
+                rightChara.material.SetFloat("_Alpha", fadeRight);
+            });
+
 
         CameraManager.Instance.ExitCameraBattle();
         
         yield return new WaitForSeconds(0.1f);
+
+        leftCharaParent.DOMoveX(originalXLeft, 0);
+        rightCharaParent.DOMoveX(originalXRight, 0);
 
         AttackUISetup();
         attackUI.gameObject.SetActive(false);
