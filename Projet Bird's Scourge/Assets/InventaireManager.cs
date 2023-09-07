a9432a93bf41f4840a079d4c1182ae5723b3bcbe
Ethan Dehay <e.dehay@rubika-edu.com>
2023-09-07 16:50:49 +0200

    Inventaire UI

--- Projet Bird's Scourge/Assets/InventaireManager.cs
+++ Projet Bird's Scourge/Assets/InventaireManager.cs
@@ -1,18 +1,63 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventaireManager : MonoBehaviour
 {
     public static InventaireManager Instance;
 
+    public List<ShopItemData> inventory;
+    
+    public List<Image> itemImageList;
+    public List<Image> infoBubbleList;
+    public List<Image> descriptionList;
+    public List<Image> nameList;
+   
+    public bool isOpen;
+    public Animator anim;
+    public GameObject fondInventaire;
+    public Vector2 openPos;
+    public Vector2 closePos;
+
     private void Awake()
     {
-        if (Instance == null)
-            Instance = this;
-        
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+
+    public void OpenCloseInentaire()
+    {
+        if (!isOpen)
+        {
+            anim.SetBool("Open",true);
+            fondInventaire.transform.DOLocalMove(openPos, 0.3f);
+        }
         else
-            Destroy(gameObject);
+        {
+            anim.SetBool("Open",false);
+            fondInventaire.transform.DOLocalMove(closePos, 0.3f);
+        }
+
+        isOpen = !isOpen;
+    }
+
+    public void UpdateInventoryUI()
+    {
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            itemImageList[i].sprite = inventory[i].image;
+            itemImageList[i].color = Color.white;
+        }
+
+      
+    }
+
+    public void UseItem(int index)
+    {
+        
     }
 }
